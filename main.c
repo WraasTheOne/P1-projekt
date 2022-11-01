9d2dd4dc453a0a5e6e1d5aa1f52b3b7a82b59f8f
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2022-11-01 10:11:42 +0100

    start fra buden

--- main.c
+++ main.c
@@ -1,42 +1,7 @@
 #include <stdio.h>
 
-struct hej
-{
-    int alder;
-    int hoejde;
-};
-
-struct dav
-{
-    int data;
-    int brede;
-};
-
-
-struct main
-{
-    char hello;
-    char test;
-    int my_int;
-    float my_flot;
-
-};
-
-struct altså
-{
-    int c;
-};
-
-
-
-
-
 int main(void){
 
-
-    printf("hej");
-    printf("mander is using clion");
-    printf("Sander was here");
     return 0;
 }
 
