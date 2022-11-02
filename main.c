5d95fd4343973d8d8d86954fc2eb60d8ab35b5b0
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2022-11-02 10:14:58 +0100

    Random generering af husstande og gader //Alex

--- main.c
+++ main.c
@@ -32,26 +32,33 @@ int main(void){
     }
     int currnt_houses = 0;
     for(int i_houses = 0; i_houses < numOfGade; ++i_houses) {
-        currnt_houses = currnt_houses + gades[i_houses].nun_houses;
+        currnt_houses += gades[i_houses].nun_houses;
     }
-    printf("anttalet of housses: %d", currnt_houses);
 
-    HOUSEHOLD* houses = malloc(currnt_houses * sizeof *houses);
+    printf("anttalet of housses: %d", currnt_houses);
+    //HOUSEHOLD *houses = malloc(numOfGade * currnt_houses * sizeof(HOUSEHOLD *));
+    HOUSEHOLD houses[numOfGade][currnt_houses];
+    //if (houses == NULL){
+    //    printf("Hov hov ");
+    //    exit(EXIT_FAILURE);
+    //}
     for (x = 0; x < numOfGade; ++x) {
-        for (int j = 0; j < gades[x].nun_houses; ++j) {
-            houses[j].curnnt_grabge = rand()%100;
-            houses[j].gade_name = x;
-            houses[j].nr = j;
+        for (int j = 0; j < currnt_houses; ++j) {
+            //houses[x][j].curnnt_grabge = rand()%100;
+            houses[x][j].gade_name = x;
+            houses[x][j].nr = j;
         }
     }
+
+
     for (x = 0; x < numOfGade; ++x) {
         printf("\n--------------------------------------\n");
         printf(" GadeNavn: %d \n antal of houses on the street %d \n lenth_of_steet of the rode %d \n "
                "lenth_of_steet of the dist of start %d \n "
-               "is open %d\n", x, gades[x].nun_houses, gades[x].lenth_of_steet, gades[x].dist_start, gades[x].open);
-        for (int y = 0; y < currnt_houses; ++y) {
+              "is open %d\n", x, gades[x].nun_houses, gades[x].lenth_of_steet, gades[x].dist_start, gades[x].open);
+        for (int y = 0; y < gades[x].nun_houses; ++y) {
             printf("| ");
-            printf("House gade: %d ", houses[y].gade_name);
+            printf("House gade: %d ", houses[x][y].gade_name);
 
 
         }
