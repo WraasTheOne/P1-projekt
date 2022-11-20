4e05d94b0699ccddefbc8bc7c20bedca673ca40c
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2022-11-20 19:47:32 +0100

    er begyndt på en test fil, hvor det kan tjekkes hvor lang en fil er //M

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct test
+{
+    int alder;
+    int højde;
+} test;
+
+
+int main(void){
+    FILE *file;
+
+    file = fopen("structs.csv", "r");
+
+    printf("");
+
+
+
+}
