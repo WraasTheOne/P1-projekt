5b7c50e3fcc903269c376c4627e8d6676f10f67a
WraasTheOne <113178081+WraasTheOne@users.noreply.github.com>
2022-11-22 11:55:03 +0100

    påbegyndelse af house-rating og indlæsning fra fil

--- new_main.c
+++ new_main.c
@@ -7,16 +7,20 @@ typedef struct struct_street{
         int length_of_street;
         int open_street;
         int distance_start;
+        int street_rating;
     }struct_street;
 typedef struct struct_house{
         int street_name;
         int house_name;
         int fill_amount_procent;
         int last_empty_days;
+        int house_rating;
     }struct_house;
 
 struct_street* generate_street_array(int amount_street);
 struct_house** generate_house_array(int amount_street, int amount_house_total, struct_street *p_array_street);
+void house_rating(struct_house** p_array_struct_house, int amount_house_total, int amount_street);
+void street_rating(struct_house** p_array_house, struct_street* p_array_street, int amount_house_total, int amount_street);
 
 int main (void){
     int amount_house_total = 0;
@@ -25,20 +29,29 @@ int main (void){
     struct_street* p_array_street;
     struct_house** p_array_house;
 
-    p_array_street = generate_street_array(amount_street);
-    //Count total amount of houses to generate arrays later
-    for (int i = 0; i < amount_street; ++i){
-        amount_house_total += p_array_street[i].amount_house_street;
+   printf("type 1 to generate data and 2 to load data");
+    scanf("%d", &choice);
+    if(choice == 1){
+        p_array_street = generate_street_array(amount_street);
+        //Count total amount of houses to generate house array
+        for (int i = 0; i < amount_street; ++i){
+            amount_house_total += p_array_street[i].amount_house_street; 
+        }
+        p_array_house = generate_house_array(amount_street, amount_house_total, p_array_street);
     }
-    p_array_house = generate_house_array(amount_street, amount_house_total, p_array_street);
-    
+    else if (choice == 2){
+        printf("WIP \n");
+        exit(0);
+    }
     //Printer
-    for (int i = 0; i < amount_street; ++i) {
+    for (int i = 0; i < amount_street; ++i){
         printf("GadeNavn: %d \n-------------\n", p_array_house[i][i].street_name);
         for (int y = 0; y < p_array_street[i].amount_house_street ; ++y) {
             printf("House_number: %d\nBin_fill_amount: %d%%\nDays since last empty: %d\n\n",p_array_house[i][y].house_name, p_array_house[i][y].fill_amount_procent, p_array_house[i][y].last_empty_days);
         }
     } 
+    house_rating(p_array_house, amount_house_total, amount_street);
+    street_rating(p_array_house, p_array_street, amount_house_total, amount_street);
     return 0;
 }
  
@@ -80,3 +93,41 @@ struct_house** generate_house_array(int amount_street, int amount_house_total,st
      }
     return (array_house);
 }
+
+void house_rating(struct_house** p_array_house, int amount_house_total, int amount_street){
+    for(int i = 0; i < amount_street; i++){
+        for(int x = 0; x < amount_house_total; x++){
+            p_array_house[i][x].house_rating = p_array_house[i][x].fill_amount_procent * p_array_house[i][x].last_empty_days; // placeholder for equation.
+            printf("House rating: %d\n", p_array_house[i][x].house_rating);
+        }
+    }
+}
+void street_rating(struct_house** p_array_house, struct_street* p_array_street, int amount_house_total, int amount_street){
+    for(int i = 0; i < amount_street; i++){
+       for(int x = 0; x < amount_house_total; x++){
+            p_array_street[i].street_rating += p_array_house[i][x].house_rating;
+        }
+    printf("Street rating: %d\n", p_array_street[i].street_rating);
+    }
+
+
+}
+
+
+
+struct_street* load_streets(){
+    int i;
+    struct_street street;
+    struct_street* array_street;
+
+    FILE *file_streets;
+    
+    
+
+    
+
+    return array_street;
+}
+
+
+
